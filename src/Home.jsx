3c6ecb95227a705c74d2ef0c8b58fd576a1dd771
Asad Ishaq <167089251+axadishaq@users.noreply.github.com>
2025-07-30 16:24:20 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import MenuSection from "./components/MenuSection";
 import FeatureBlog from "./components/FeatureBlog";
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
-import ReservationSection from "./components/ReservationSection";
+import { ReservationSection } from "./components/ReservationSection";
 
 export const Home = () => {
    return (
